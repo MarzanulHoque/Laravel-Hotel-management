9bfa58fc2175f9e6e21cd768fb4e7e7000196193
marzanulhoque <marzanulru17@gmail.com>
2024-01-02 20:31:27 +0600

    Admin Logout Functionality added

--- hotel/app/Http/Controllers/AdminController.php
+++ hotel/app/Http/Controllers/AdminController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -9,5 +9,16 @@ class AdminController extends Controller
     public function AdminDashboard()
     {
         return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
